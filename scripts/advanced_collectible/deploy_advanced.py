711eda39718976521b8421ef6f4e29cddccb3f20
Austin Burke <austinlayneburke@gmail.com>
2021-11-03 05:55:37 -0500

    Updating deploy script

--- scripts/advanced_collectible/deploy_advanced.py
+++ scripts/advanced_collectible/deploy_advanced.py
@@ -1,4 +1,5 @@
 from brownie import AdvancedCollectible, accounts, network, config
+from scripts.helpful_scripts import fund_advanced_collectible
 
 
 def main():
@@ -6,10 +7,12 @@ def main():
     print(dev)
     print(network.show_active())
     publish_source = False
-    advancedCollectible = AdvancedCollectible.deploy(
+    advanced_collectible = AdvancedCollectible.deploy(
         config["networks"][network.show_active()]["vrf_coordinator"],
         config["networks"][network.show_active()]["link_token"],
         config["networks"][network.show_active()]["keyhash"],
         {"from": dev},
         publish_source=publish_source,
     )
+    fund_advanced_collectible(advanced_collectible)
+    return advanced_collectible
